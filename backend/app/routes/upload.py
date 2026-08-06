751bef8fe563d7688457911f66c3f70bbd25e319
yuvrajsingh0125 <yuvismail2802@gmail.com>
2026-08-07 01:49:41 +0530

    Backend : structuring and image upload/save

--- backend/app/routes/upload.py
+++ backend/app/routes/upload.py
@@ -1,5 +1,6 @@
 #  this entire file is for testing the upload functionality. It will be removed later when the upload functionality is implemented in the frontend.
 from fastapi import APIRouter, UploadFile, File
+from backend.app.services.opencv_services import process_image
 from app.utils.file_utils import save_uploaded_file
 
 router = APIRouter(prefix="/upload", tags=["Upload"])
@@ -9,10 +10,13 @@ router = APIRouter(prefix="/upload", tags=["Upload"])
 async def upload_image(file: UploadFile = File(...)):
     # save the uploaded file to the uploads directory
     saved_path = save_uploaded_file(file)
+    
+    processed_path = process_image(saved_path)
 
     return {
         # this message will be displayed in the frontend after the file is uploaded successfully
         "message": "Image uploaded successfully",
         "filename": file.filename,
-        "saved_path": saved_path
+        "saved_path": saved_path,
+        "processed_path": processed_path
     }
---
+++ backend/app/services/documentai_service.py
@@ -0,0 +1,6 @@
+def extract_text(image_path: str):
+    """
+    Placeholder.
+    Arshdeep will implement Google Document AI here.
+    """
+    return None
